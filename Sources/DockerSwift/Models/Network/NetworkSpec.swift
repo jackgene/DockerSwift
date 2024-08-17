f13cfb44fbe56747428b915e020473d0c2bdb972
Matthieu Barthelemy <bonsouere@gmail.com>
2024-08-17 10:47:23 +1000

    Support Podman unix signals

--- Sources/DockerSwift/Models/Network/NetworkSpec.swift
+++ Sources/DockerSwift/Models/Network/NetworkSpec.swift
@@ -45,7 +45,7 @@ public struct NetworkSpec: Codable {
         self.labels = labels
     }
     
-    enum CodingKeys: String, CodingKey {
+    public enum CodingKeys: String, CodingKey {
         case name = "Name"
         case checkDuplicate = "CheckDuplicate"
         case driver = "Driver"
